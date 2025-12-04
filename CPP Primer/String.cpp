728114da4c0bde5f48e3d75719bd8234fe408697
Rudransh Kala <rudranshkala06@gmail.com>
2025-12-04 16:16:40 +1100

    Work from today

--- CPP Primer/String.cpp
+++ CPP Primer/String.cpp
@@ -21,16 +21,34 @@ int main(){
         cout << word << endl;
     }
     */
-    
+
+    /*
     string line;
     while (getline(cin, line)){
         if (!line.empty()){
             cout << line << endl;
+
+            if (line.size() > 80){
+                cout << "WoW, This line was over 80 characters long!" << endl;
+            }
+
             continue;
         }
         
         cout << "Must include a valid line to be printed out" << endl;
     }
+    */
+
+    string random = "hello, How are you?"; 
+    string &temp = random;
+
+    temp = "ahahahaha";
+
+    for (auto &c: random){
+        c = toupper(c);
+    }
+
+    cout << temp << endl;
 
     return 0;
 }
