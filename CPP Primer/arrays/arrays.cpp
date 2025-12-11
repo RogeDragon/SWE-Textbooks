987beda3134fa0add8759ed9f899c1d483e66834
Rudransh Kala <rudranshkala06@gmail.com>
2025-12-11 14:14:15 +1100

    started working on learning arrays

---
+++ CPP Primer/arrays/arrays.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+using std::string;
+
+int main(){
+    int *Pointers[10]; //creates an array of int pointers
+    int (*ArrayPointer)[10]; // a pointer that pointe to an array of ten elements
+
+
+    //int ai[7*4];
+
+    return 0;
+}
--- CPP Primer/vectors/vector.cpp
+++ CPP Primer/vectors/vector.cpp
@@ -42,7 +42,5 @@ int main(){
     }
     cout << endl;
 
-
-
     return 0;
 }
